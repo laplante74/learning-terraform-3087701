d48f006f8d392fd96720f8c7e383f9a2a3355788
laplante74 <121687633+laplante74@users.noreply.github.com>
2024-11-04 16:52:19 -0500

    fix syntx error

--- main.tf
+++ main.tf
@@ -22,7 +22,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_groups = [aws_security_group.blog.id]
+  vpc_security_groups_id = [aws_security_group.blog.id]
 
   tags = {
     Name = "HelloWorld"
@@ -40,7 +40,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   type        = "ingress"
   from_port   = 80
   to_port     = 80
-  protocol    = tcp
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
@@ -50,7 +50,7 @@ resource "aws_security_group_rule" "blog_https_in" {
   type        = "ingress"
   from_port   = 443
   to_port     = 443
-  protocol    = tcp
+  protocol    = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
   security_group_id = aws_security_group.blog.id
