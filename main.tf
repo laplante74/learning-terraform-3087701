97a86caa9f09ab7fab22c692cd83bb20fa46c14e
laplante74 <121687633+laplante74@users.noreply.github.com>
2024-11-05 12:34:51 -0500

    typo

--- main.tf
+++ main.tf
@@ -60,13 +60,11 @@ module "alb" {
       target_id        = aws_instance.blog.id
     }
 
-  http_tcp_listeners = [
-    {
+  http_tcp_listeners = {
       port     = 80
       protocol = "HTTP"
       target_group_index = 0
       }
-  ]
 
   tags = {
     Environment = "Dev"
